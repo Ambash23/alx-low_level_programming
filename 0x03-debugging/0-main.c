9dc69b1a90f774c838a80a761e0d81b62da14fad
Ambash23 <akanbibashir63@gmail.com>
2022-07-07 16:54:34 -0700

    tasj

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,9 +1,9 @@
-nclude "main.h"
+include "main.h"
 
 /**
-** main - tests function that prints if integer is positive or negative
-** Return: zero
-**/
+ * main - tests function that prints if integer is positive or negative
+ * Return: zero
+ */
 
 int main(void)
 {
